a2722ee4366d62df9e23eddb865e701e9a04b32b
Simon Myhill <myhillsimon96@gmail.com>
2025-03-22 19:29:27 -0400

    template out homepage

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue';
+
+export default defineComponent({
+    name: 'Home',
+    setup() {
+        const message = ref('Loading');
+
+        onMounted(async () => {
+            const response = await fetch('http://localhost:8080/my-name');
+            const data = await response.json();
+            message.value = data.message;
+        });
+
+        return { message};
+    }
+});
+
+</script>
